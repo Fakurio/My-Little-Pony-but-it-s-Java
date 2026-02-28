b927ffad4c392455d97e90162f6216ed44e32533
Fakurio <kamil.zelazowski.2@gmail.com>
2026-02-28 11:04:55 +0100

    Spring Boot initial

---
+++ src/test/java/com/unicorn/my_little_pony/MyLittlePonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicorn.my_little_pony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittlePonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
