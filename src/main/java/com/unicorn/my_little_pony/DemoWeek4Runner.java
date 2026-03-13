3e4fd73f054c38e1366a2cfd5cd1baafbd4e27be
Fakurio <kamil.zelazowski.2@gmail.com>
2026-03-13 16:35:51 +0100

    fasada pyłkowego pośrednika

---
+++ src/main/java/com/unicorn/my_little_pony/DemoWeek4Runner.java
@@ -0,0 +1,11 @@
+package com.unicorn.my_little_pony;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DemoWeek4Runner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
