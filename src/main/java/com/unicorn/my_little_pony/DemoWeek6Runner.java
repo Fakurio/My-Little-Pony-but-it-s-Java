3b55d622f954901797dfaaae38ecc8666defa6f6
Fakurio <kamil.zelazowski.2@gmail.com>
2026-04-11 11:07:12 +0200

    SO so so

--- src/main/java/com/unicorn/my_little_pony/DemoWeek6Runner.java
+++ src/main/java/com/unicorn/my_little_pony/DemoWeek6Runner.java
@@ -60,18 +60,18 @@ public class DemoWeek6Runner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("\n");
-        System.out.println("===========================================================");
-        System.out.println("  DEMO TYDZIEŃ 6: Strategy + State + Observer + Visitor + Template ");
-        System.out.println("===========================================================");
-        System.out.println();
-        demoStrategy();
-        demoObserver();
-        demoState();
-        demoVisitor();
-        demoTemplate();
-        System.out.println();
-        System.out.println("===========================================================");
+//        System.out.println("\n");
+//        System.out.println("===========================================================");
+//        System.out.println("  DEMO TYDZIEŃ 6: Strategy + State + Observer + Visitor + Template ");
+//        System.out.println("===========================================================");
+//        System.out.println();
+//        demoStrategy();
+//        demoObserver();
+//        demoState();
+//        demoVisitor();
+//        demoTemplate();
+//        System.out.println();
+//        System.out.println("===========================================================");
     }
 
     private void demoTemplate() {
---
+++ src/main/java/com/unicorn/my_little_pony/DemoWeek7Runner.java
@@ -0,0 +1,19 @@
+package com.unicorn.my_little_pony;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoWeek7Runner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("\n");
+        System.out.println("===========================================================");
+        System.out.println("  DEMO TYDZIEŃ 7: SRP + Open close ");
+        System.out.println("===========================================================");
+        System.out.println();
+        System.out.println();
+        System.out.println("===========================================================");
+    }
+}
