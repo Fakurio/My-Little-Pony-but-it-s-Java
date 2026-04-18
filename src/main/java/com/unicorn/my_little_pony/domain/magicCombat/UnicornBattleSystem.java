acca184de6959ba148ec304ceb10b6e11add5b48
Fakurio <kamil.zelazowski.2@gmail.com>
2026-04-18 12:14:12 +0200

    wrung liczba

--- src/main/java/com/unicorn/my_little_pony/domain/magicCombat/UnicornBattleSystem.java
+++ src/main/java/com/unicorn/my_little_pony/domain/magicCombat/UnicornBattleSystem.java
@@ -2,7 +2,7 @@ package com.unicorn.my_little_pony.domain.magicCombat;
 
 import com.unicorn.my_little_pony.domain.magicCombat.attacks.MagicAttack;
 
-//Tydzień 8, DI, Zastosowanie 2
+//Tydzień 8, DI, Zastosowanie 3
 //Moduł wysokopoziomowy wywołuje atak magiczny poprzez interfejs nie znając rodzaju użytej magii
 public class UnicornBattleSystem {
     public void castSpell(MagicAttack attack) {
